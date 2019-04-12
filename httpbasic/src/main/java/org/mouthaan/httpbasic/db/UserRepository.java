0525e6fcf068920cf93c1ef2ca06905fbdab975c
raymondmouthaan <raymondmmouthaan@gmail.com>
2019-04-12 22:21:40 +0200

    http(s)basic app added

---
+++ httpbasic/src/main/java/org/mouthaan/httpbasic/db/UserRepository.java
@@ -0,0 +1,10 @@
+package org.mouthaan.httpbasic.db;
+
+import org.mouthaan.httpbasic.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
---
+++ httpbasic/src/main/java/org/mouthaan/httpbasic/security/UserPrincipalDetailsService.java
@@ -0,0 +1,26 @@
+package org.mouthaan.httpbasic.security;
+
+import org.mouthaan.httpbasic.db.UserRepository;
+
+import org.mouthaan.httpbasic.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserPrincipalDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserPrincipalDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(s);
+        return new UserPrincipal(user);
+    }
+}
