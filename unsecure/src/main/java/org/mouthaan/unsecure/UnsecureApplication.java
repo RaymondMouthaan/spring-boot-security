c354ca4b455eca72ff40824061f566a2ad0462ed
raymondmouthaan <raymondmmouthaan@gmail.com>
2019-04-12 21:09:56 +0200

    unsecure app added

---
+++ unsecure/src/main/java/org/mouthaan/unsecure/UnsecureApplication.java
@@ -0,0 +1,11 @@
+package org.mouthaan.unsecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsecureApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UnsecureApplication.class, args);
+    }
+}
