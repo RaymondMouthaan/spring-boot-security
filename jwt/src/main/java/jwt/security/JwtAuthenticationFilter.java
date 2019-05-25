568462f57af4a7b5671e0b96732e5f27de5342f9
raymondmouthaan <raymondmmouthaan@gmail.com>
2019-05-25 13:14:07 +0200

    - catched up to #29 JWT - Implement JWT Authentication

---
+++ jwt/src/main/java/jwt/security/JwtAuthenticationFilter.java
@@ -0,0 +1,69 @@
+package jwt.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jwt.model.LoginViewModel;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private AuthenticationManager authenticationManager;
+
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    /* Trigger when we issue POST request to /login
+     * We also need to pass in {"username":"dan", "password":"dan123"} in the request body
+     */
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        // Grab credentials and map them to loginViewModel
+        LoginViewModel credentials = null;
+
+        try {
+            credentials = new ObjectMapper().readValue(request.getInputStream(), LoginViewModel.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Create login token
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                credentials.getUsername(),
+                credentials.getPassword(),
+                new ArrayList<>());
+
+        // Authenticate user
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
+        return authentication;
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        // Grab principles
+        UserPrincipal principal = (UserPrincipal) authResult.getPrincipal();
+
+        // Create JWT Token
+        String token = JWT.create()
+                .withSubject(principal.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME))
+                .sign(Algorithm.HMAC512(JwtProperties.SECRET.getBytes()));
+
+        // Add token in response
+        response.addHeader(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX + token);
+    }
+}
---
+++ jwt/src/main/java/jwt/security/UserPrincipalDetailsService.java
@@ -0,0 +1,25 @@
+package jwt.security;
+
+import jwt.db.UserRepository;
+import jwt.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserPrincipalDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserPrincipalDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(s);
+        return new UserPrincipal(user);
+    }
+}
